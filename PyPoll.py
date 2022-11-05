5114395be3150735b9d671178c8eb75ea2ade02b
RyanDevillier <ryancdevillier@gmail.com>
2022-11-05 18:38:34 -0500

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -68,11 +68,92 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# Declaring an accumulator variable.
+total_votes = 0
+
+# Declaring an empty list to hold the different candidates' names.
+candidate_options = []
+
+# Declaring an empty dictionary to hold the individual candidates' votes.
+candidate_votes = {}
+
+# Declaring a variable that holds a place for the winning candidate.
+winning_candidate = ""
+
+# Declaring a variable for the winning count 
+winning_count = 0
+
+# Declaring a variable for the winning percentage 
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
     file_reader = csv.reader(election_data)
 
-    # Printing the header row. 
+    # Read the header row. 
     headers = next(file_reader)
-    print(headers)
+    
+    # Iterate through each row in the CSV file.
+    for row in file_reader:
+
+        # Adding to the total vote count.
+        total_votes +=1
+
+        # Gets the candidate's name from each row.
+        candidate_name = row[2]
+
+        # Adds the unique candidate's name to the candidate list.
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+
+            # Initializes all the candidates' votes to zero.
+            candidate_votes[candidate_name] = 0
+
+            # Adds the votes per candidate.
+        candidate_votes[candidate_name] += 1
+
+# Prints the total votes.
+# print(f"1.) This is the total number of votes in the election: {total_votes}")
+
+# Prints the candidates names.
+# print(f"2.) These are all the candidates in the election: {candidate_options}")
+
+# Prints the number of votes each candidate received.
+# print(f"3.) This is the number of votes each candidate received in the election: {candidate_votes}")
+
+# Two different approaches of coming up with the percentage of votes per candidate. (Not sure if one is more beneficial to use than the other, but the first approach is what I came up with before looking at the solution in the module.)
+
+# for candidate_name in candidate_options:
+    # percentage_votes = (candidate_votes[candidate_name] / total_votes * 100)
+    # print(f"{candidate_name} received {percentage_votes:.2f}% of the total vote.")
+
+
+# Prints the percentage of votes each candidate received.
+for candidate_name in candidate_votes:
+
+    votes = candidate_votes[candidate_name]
+
+    vote_percentage = float(votes) / float(total_votes) * 100
+
+    #print(f"{candidate_name} received {vote_percentage:.2f}% of the total vote.")
+
+    print(f"{candidate_name}: {vote_percentage:.1f} ({votes:,})\n\n")
+
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+
+        winning_count = votes
+
+        winning_percentage = vote_percentage
+
+        winning_candidate = candidate_name
+
+winning_candidate_summary = (
+    
+    f"---------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}\n"
+    f"---------------------------\n")
+
+print(winning_candidate_summary)
